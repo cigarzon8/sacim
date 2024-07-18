6c47ab27bbafdedb4ca8855b159daf6fb9a16754
amplifyproyects <amplifyproyects@gmail.com>
2024-07-18 17:08:52 -0500

    add form to create use

--- node/user/routes.js
+++ node/user/routes.js
@@ -7,13 +7,12 @@ const auth = require('../midleware/auth')
 router.get('/', async function(req, res) {
   const users = await User.findAll();
   const usersData = users.map(user => user.toJSON());
-  res.render('list', {datalist:usersData});
+  const values = ["#","Estado","Nombres","Apellidos","Correo","Documento"]
+  res.render('user/list', {datalist:usersData,headerlist:values});
 });
 
 router.get('/bienvenido',auth, async function(req, res) {
-  const users = await User.findAll();
-  const usersData = users.map(user => user.toJSON());
-  res.render('bienvenido', {datalist:usersData});
+  res.render('bienvenido');
 });
 
 router.post('/auth', async function(req, res) {
@@ -27,6 +26,17 @@ router.get('/logout', async function(req, res) {
   req.session.destroy(function(){
     res.redirect('/');
   });
+});
+
+router.get('/add', async function(req, res) {
+  res.render('user/add');
+});
+
+router.post('/add', async function(req, res) {
+  req.body.estado = 1
+  const user = new User(req.body);
+  user.save()
+  res.redirect('/user')
 });
 
 async function authenticate(name, pass, fn) {
