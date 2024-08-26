566a8b887824f6851c490c6c80eac82e4b6cde3c
Juan carlos <osoriovargasjuan01@gmail.com>
2024-08-26 12:39:07 -0500

    agregando entidades en archivo routes.js

--- appi/routes.js
+++ appi/routes.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../model/User');
+const Estado = require('../model/Estado');
+const Cobro = require('../model/Cobro');
+const Parqueadero = require('../model/Parqueadero');
+const Vehiculo = require('../model/Vehiculo');
+
 const auth = require('../midleware/auth')
 
 router.get('/', async function (req, res) {
@@ -16,10 +21,34 @@ router.post('/auth', async function (req, res) {
 });
 
 
-router.get('/listaUsuarios', async function (req, res) {
+router.get('/listausuarios', async function (req, res) {
     const users = await User.findAll();
     const user = users.map(user => user.toJSON());
     res.json({ Message: 'okay', user })
+});
+
+router.get('/listaestados', async function (req, res) {
+    const estados = await Estado.findAll();
+    const estadoslist = estados.map(data => data.toJSON());
+    res.json({ Message: 'okay', estadoslist })
+});
+
+router.get('/listacobros', async function (req, res) {
+    const cobros = await Cobro.findAll();
+    const cobroslist = cobros.map(data => data.toJSON());
+    res.json({ Message: 'okay', cobroslist })
+});
+
+router.get('/listaparqueaderos', async function (req, res) {
+    const parqueaderos = await Parqueadero.findAll();
+    const parqueaderoslist = parqueaderos.map(data => data.toJSON());
+    res.json({ Message: 'okay', parqueaderoslist })
+});
+
+router.get('/listavehiculos', async function (req, res) {
+    const vehiculos = await Vehiculo.findAll();
+    const vehiculoslist = vehiculos.map(data => data.toJSON());
+    res.json({ Message: 'okay', vehiculoslist })
 });
 
 
