a8d8991ef171d7735b2ad18ec4b713a17aaf278c
cigarzon <cigarzon@rcnradio.com.co>
2025-10-07 21:23:01 -0500

    implement db postgres

--- db.js
+++ db.js
@@ -1,8 +1,29 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('sacimdatabase', 'sacim', 'DATABASE$*b123Pas456abc', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+let sequelize;
+
+if (process.env.RENDER_DB_URL) {
+  // 🔹 Render: PostgreSQL
+  sequelize = new Sequelize(process.env.RENDER_DB_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false
+  });
+  console.log('🔗 Usando base de datos PostgreSQL (Render)');
+} else {
+  // 🔹 Local o externa: MySQL
+  sequelize = new Sequelize('sacimdatabase', 'sacim', 'DATABASE$*b123Pas456abc', {
+    host: 'localhost',
+    dialect: 'mysql',
+    logging: false
+  });
+  console.log('🔗 Usando base de datos MySQL (local)');
+}
 
 module.exports = sequelize;
