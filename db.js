acb72b019aa0f2c86488e90bd80351e619aadd21
cigarzon <cigarzon@rcnradio.com.co>
2025-10-07 21:43:31 -0500

    implement logs

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const { Sequelize } = require('sequelize');
 
 const DATABASE_URL = process.env.DATABASE_URL || 'mysql://sacim:DATABASE$*b123Pas456abc@localhost:3306/sacimdatabase';//''postgres://usuario:contraseña@host:5432/nombre_base';
+console.log('DATABASE_URL,',DATABASE_URL)
 const DATABASE_dialect = process.env.DATABASE_URL || 'mysql';//''postgres://usuario:contraseña@host:5432/nombre_base';
+console.log('DATABASE_dialect,',DATABASE_dialect)
 const sequelize = new Sequelize(DATABASE_URL, {
   dialect: DATABASE_dialect,
   logging: false
