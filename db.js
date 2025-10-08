acae0fda47001f9eb1c32f43759eaea687de91e1
cigarzon <cigarzon@rcnradio.com.co>
2025-10-07 21:34:43 -0500

    implement deffaut contect

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');
 
 const DATABASE_URL = process.env.DATABASE_URL || 'mysql://sacim:DATABASE$*b123Pas456abc@localhost:3306/sacimdatabase';//''postgres://usuario:contraseña@host:5432/nombre_base';
-
-const sequelize = new Sequelize('sacimdatabase', 'sacim', 'DATABASE$*b123Pas456abc', {
-  host: 'localhost',
-  dialect: 'mysql'
+const DATABASE_dialect = process.env.DATABASE_URL || 'mysql';//''postgres://usuario:contraseña@host:5432/nombre_base';
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialect: DATABASE_dialect,
+  logging: false
 });
 
 module.exports = sequelize;
