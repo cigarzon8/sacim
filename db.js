e7469125e11bd2eb43a34343b5d8243f40f5f932
cigarzon <cigarzon@rcnradio.com.co>
2025-11-11 15:55:24 -0500

    implement database complete

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const DATABASE_URL = process.env.DATABASE_URL || 'mysql://sacim:DATABASE$*b123Pas456abc@localhost:3306/sacimdatabase2';//''postgres://usuario:contraseña@host:5432/nombre_base';
+const DATABASE_URL = process.env.DATABASE_URL || 'mysql://sacim:DATABASE$*b123Pas456abc@localhost:3306/sacimdatabase112025';//''postgres://usuario:contraseña@host:5432/nombre_base';
 console.log('DATABASE_URL,',DATABASE_URL)
 const DATABASE_dialect = process.env.DATABASE_URL || 'mysql';//''postgres://usuario:contraseña@host:5432/nombre_base';
 console.log('DATABASE_dialect,',DATABASE_URL)
